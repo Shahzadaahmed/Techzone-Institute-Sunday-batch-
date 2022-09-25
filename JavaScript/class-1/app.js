476d128f4edaa939b075ec0af5ee1cc56454d473
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-09-25 15:50:14 +0500

    Exercise updated

---
+++ JavaScript/class-1/app.js
@@ -0,0 +1,24 @@
+// var myName = "ahmed";
+// console.log(myName);
+// alert(myName);
+
+
+// var firstName = "muhammad";
+// var lastName = 'ahmed';
+
+// alert(firstName);
+// alert(lastName);
+
+// console.log(firstName);
+// console.log(lastName);
+
+// console.log(typeof (firstName));
+// console.log(typeof (lastName));
+
+
+// var myName = "ahmed";
+// myName = "muhammad ahmed";
+// console.log(myName);
+
+var str = "muhammad\nahmed";
+console.log(str);
