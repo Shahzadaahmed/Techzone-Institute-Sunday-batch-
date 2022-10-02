ede437872a2904b5d1b3f4811fd5058e6e348ffc
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-10-02 15:32:48 +0500

    It statement topic done

--- JavaScript/class-1/app.js
+++ JavaScript/class-1/app.js
@@ -20,5 +20,71 @@
 // myName = "muhammad ahmed";
 // console.log(myName);
 
-var str = "muhammad\nahmed";
-console.log(str);
+// var str = "muhammad\nahmed";
+// console.log(str);
+
+// var num = "1996";
+// console.log(num);
+// console.log(typeof (num));
+
+// var actualNum = 8;
+// console.log(actualNum);
+// console.log(typeof actualNum);
+
+
+
+// var firstVal = 5;
+// var lastVal = 10;
+// var total = firstVal + lastVal;
+// console.log(total);
+
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// console.log(firstName, lastName);
+
+// var fullName = firstName + " " + lastName;
+// console.log("My Full Name: ", fullName);
+
+
+
+// var firstVal = "5";
+// var lastVal = "10";
+// var total = firstVal + lastVal;
+// console.log(total);
+
+// var myNameIsHuzaifa = "ahmed"; // only camel case and start with under score with _
+// console.log(myNameIsHuzaifa);
+
+// var titleMessage;
+// titleMessage = "Hello World!";
+// console.log(titleMessage);
+
+// var firstVal = 4;
+// var secondVal;
+// secondVal = 6;
+// console.log(firstVal + secondVal);
+
+
+// var firstVal = 18;
+// var LastVal = 10;
+// var subtract = firstVal - LastVal;
+// console.log(subtract);
+
+
+// var firstVal = 8;
+// var LastVal = 10;
+// var multiply = firstVal * LastVal;
+// console.log(multiply);
+
+
+// var firstVal = 10;
+// var LastVal = 2;
+// var divide = firstVal / LastVal;
+// console.log(divide);
+
+
+var firstVal = 9;
+var LastVal = 3;
+var reminder = firstVal % LastVal;
+console.log(reminder);
---
+++ JavaScript/class-2/app.js
@@ -0,0 +1,139 @@
+// var equation = (2 * 2) + (3 + 7);
+// console.log(equation);
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+// var firstVal = "2";
+// var lastVal = "8";
+// var val = firstVal + lastVal;
+// console.log(val);
+
+// var firstInput = prompt("What is your first name?");
+// var lastInput = prompt("What is your last name?");
+// console.log(firstInput, lastInput);
+
+
+// Note: If Statement Topic...!
+
+// var firstName = "muhammad";
+// var lastName = "ahmed";
+// var age = "26";
+// var num = 8
+
+// console.log(age);
+// console.log(typeof age);
+
+// console.log(num);
+// console.log(typeof num);
+
+// == only for values check...!
+// === for values and data types check...!
+
+// var userInput = prompt('Enter any number');
+// console.log(userInput);
+// console.log(typeof userInput);
+
+// var num = "2";
+// num = Number(num);
+// console.log(num);
+// console.log(typeof num);
+
+// var num = Number(prompt('Enter any number'));
+// console.log(num);
+
+// if (userInput == "ahmed") {
+//     console.log('Name Matched!');
+// }
+
+// else {
+//     console.log('Invalid Name!');
+// };
+
+
+// var userNum = Number(prompt('Enter any number'));
+
+// if (userNum == "4") {
+//     console.log(userNum + 4);
+// }
+
+// else {
+//     console.log('Condition did not match!');
+// };
+
+// || or operator...!
+
+// var userInput = prompt('Enter any name');
+
+// if ((userInput == "muhammad") || (userInput == "ahmed")) {
+//     console.log(userInput);
+// }
+
+// else {
+//     console.log('Not Matched!');
+// };
+
+
+// && or operator...!
+
+// var userInput = Number(prompt('Enter any number'));
+
+// if ((userInput >= 5) && (userInput <= 10)) {
+//     console.log('Matched');
+// }
+
+// else {
+//     console.log('Not Matched!');
+// };
+
+// var firstVal = 3;
+// var lastVal = 6;
+// console.log();
+
+// var userInput = prompt('Enter any name');
+
+// if (userInput != "ahmed") {
+//     console.log(false);
+// }
+
+// else if (userInput == "ali") {
+//     console.log('Nick name');
+// }
+
+// else {
+//     console.log(true);
+// };
+
+// var userInput = Number(prompt('Enter any number'));
+
+// if (userInput == 8) {
+//     console.log('Date of Birth!');
+// }
+
+// else if (userInput == 7) {
+//     console.log('Month of Birth!');
+// }
+
+// else {
+//     console.log('Invalid input!');
+// };
+
+var userInput = Number(prompt('Enter any number'));
+var luckNum = 8;
+
+if (userInput >= 5) {
+    if (userInput == luckNum) {
+        console.log(userInput + luckNum);
+    }
+
+    else {
+        console.log('Lucky Number Not Matched!')
+    }
+}
+
+else {
+    console.log('Number Not Matched!');
+};
