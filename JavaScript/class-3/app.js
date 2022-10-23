cc2c65950760d49337f18edd7aa92397edecb7d5
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-10-23 16:13:45 +0500

    Arrays updated

---
+++ JavaScript/class-3/app.js
@@ -0,0 +1,121 @@
+// Question # 6:
+/*
+var userInput = prompt('Enter any character or number!');
+// console.log(userInput);
+// console.log(typeof userInput);
+
+if (userInput > 0 || userInput < 0) {
+    console.log(userInput, "is number");
+}
+
+else if (userInput == "A" || userInput == "B" || userInput == "C") {
+    console.log(userInput, "is an uppercase character");
+}
+
+else if (userInput == "a" || userInput == "b" || userInput == "c") {
+    console.log(userInput, "is an lowercase character");
+}
+
+else {
+    console.log('Invalid Input!');
+};
+*/
+
+// Question # 07:
+/*
+var firstNum = Number(prompt("Enter first number!"));
+var secondNum = Number(prompt("Enter second number!"));
+var operator = prompt("Enter operator!");
+
+if (operator == "+") {
+    console.log(firstNum + secondNum);
+}
+*/
+
+// Question # 09:
+/*
+var userInput = prompt("Enter any year!");
+
+if (userInput % 4 == 0) {
+    console.log(userInput, "is a leap year!")
+}
+
+else {
+    console.log(userInput, "is not a leap year!")
+};
+*/
+
+// Question # 13:
+/*
+var firstNum = Number(prompt("Enter first number!"));
+var secondNum = Number(prompt("Enter second number!"));
+
+if (firstNum > secondNum) {
+    console.log('First num is greater!');
+}
+
+else if (firstNum < secondNum) {
+    console.log('Second num is greater!');
+}
+
+else if (firstNum == secondNum) {
+    console.log("Both integers are equal!");
+};
+*/
+
+// Question # 14:
+/*
+var num = Number(prompt("Enter any number!"));
+
+if (num > 0) {
+    console.log(num, "is greater than 0");
+}
+
+else if (num < 0) {
+    console.log(num, "is less than 0");
+}
+
+else if (num == 0) {
+    console.log(num, "is 0");
+};
+*/
+
+
+// var bool = false;
+// console.log(bool);
+
+// var myName = "ahmed";
+// myName = undefined;
+// console.log(myName);
+
+// Question # 16:
+
+// var myData = "ahmed";
+// var myData = 8;
+// var myData = true;
+// var myData;
+
+/*
+var myData = prompt("Enter any data!");
+console.log(myData, typeof myData);
+
+if (typeof myData == "string") {
+    console.log('Your data type is string!');
+}
+
+else if (typeof myData == "number") {
+    console.log('Your data type is number!');
+}
+
+else if (typeof myData == "boolean") {
+    console.log('Your data type is boolean!');
+}
+
+else if (typeof myData == "undefined") {
+    console.log('Your data type is undefined!');
+}
+
+else {
+    console.log('Invalid Input!');
+};
+*/
---
+++ JavaScript/class-3/array/app.js
@@ -0,0 +1,51 @@
+// var arr = [
+//     "ahmed",
+//     "mehran",
+//     "kashan",
+//     "shahzaib",
+//     "khurram",
+//     6,
+//     true,
+//     "faraz",
+//     undefined,
+//     "",
+//     0,
+//     false
+// ];
+// console.log(arr);
+// console.log(arr.length);
+
+// var checkIndex = arr[4];
+// console.log(checkIndex);
+
+
+// var fruitsArr = ["apple", "banana", "mango"];
+// fruitsArr.push("grapes"); // To add in last...!
+// fruitsArr.pop(); // To remove from last...!
+// fruitsArr.unshift(200); // To add in start...!
+// fruitsArr.shift(); // To remove from start...!
+// console.log(fruitsArr);
+
+// var petsArr = [];
+// petsArr[0] = "dog";
+// petsArr[1] = "cat";
+// petsArr[3] = "parrot";
+// console.log(petsArr[2]);
+
+// var charsArr = ['a', 'b', 'c', 'd', 'e', 'b', 'w'];
+// var dataAt = charsArr.indexOf('z');
+// console.log(dataAt);
+
+
+// var fruitsArr = ["apple", "banana", "mango", "grapes", "strawberry"];
+// console.log(fruitsArr);
+
+// var copyArr = fruitsArr.slice(1, 2);
+// console.log(copyArr);
+
+
+var fruitsArr = ["apple", "banana", "mango", "grapes", "strawberry"];
+console.log(fruitsArr);
+
+fruitsArr.splice(0, 2, "ahmed");
+console.log(fruitsArr);
