507d8e19edc0163382072f456c4ce2aec6b56ad4
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-06 15:59:42 +0500

    Loop exercise updated

---
+++ JavaScript/class-5/app.js
@@ -0,0 +1,102 @@
+// for (var i = 0; i < 10; i++) {
+//     console.log(i);
+// };
+
+
+// for (var i = 10; i <= 30; i++) {
+//     console.log(i);
+// };
+
+
+// var names = ["ahmed", "mehran", "kashan", "shahzaib", "khurram"];
+// console.log(names);
+// console.log(names.length - 1);
+
+// for (var i = 0; i < names.length; i++) {
+//     // console.log(names[i]);
+//     console.log(i);
+// };
+
+
+
+
+// for (var i = 10; i >= 5; i--) {
+//     console.log(i);
+// }
+
+
+// for (var i = names.length - 1; i >= 0; i--) {
+//     console.log(names[i], i);
+// };
+
+
+
+// for (var i = 5; i <= 20; i++) {
+//     console.log(i);
+
+//     if (i == 10) {
+//         break;
+//     };
+// };
+
+
+// var citiesList = ["karachi", "lahore", "multan", "islamabad", "peshawar"];
+// // console.log(citiesList);
+
+// var userInput = prompt("Please enter the name of your fav city!");
+// // console.log(userInput);
+
+
+// for (var i = 0; i < citiesList.length; i++) {
+//     // console.log(citiesList[i]);
+
+//     if (citiesList[i] == userInput) {
+//         console.log('City found successfully!');
+//         break;
+//     }
+
+//     else {
+//         console.log('City not found!');
+//     };
+// };
+
+
+/*
+var citiesList = ["karachi", "lahore", "multan", "islamabad", "peshawar"];
+var userInput = prompt("Please enter the name of your fav city!");
+var cityFound = false;
+var targetCity = undefined;
+
+for (var i = 0; i < citiesList.length; i++) {
+    // console.log(citiesList[i]);
+
+    if (citiesList[i] == userInput) {
+        cityFound = true;
+        targetCity = citiesList[i];
+        break;
+    };
+};
+
+if (!cityFound) {
+    console.log('City not found!');
+}
+
+else {
+    console.log(targetCity);
+};
+*/
+
+// var citiesList = ["karachi", "lahore", "multan", "islamabad", "peshawar"];
+
+// for (var i = 0; i < citiesList.length; i++) {
+//     console.log(citiesList[i]);
+
+//     for (var j = 1; j <= 5; j++) {
+//         console.log(j);
+//     };
+// };
+
+
+for ( var i = 10; i >= 5; i-- ) {
+    console.log(i);
+};
