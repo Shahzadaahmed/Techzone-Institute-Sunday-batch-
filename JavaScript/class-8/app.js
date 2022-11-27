22de7f66df2abd75a617bbde38e09093323204ba
Shahzadaahmed <shahzada.ahmed1996@gmail.com>
2022-11-27 16:40:52 +0500

    Code updated

---
+++ JavaScript/class-8/app.js
@@ -0,0 +1,107 @@
+// var roundVal = Math.round(2.5);
+// console.log(roundVal);
+
+
+// console.log(Math.round(2.51));
+// console.log(Math.round(9.435));
+// console.log(Math.round(5.510));
+// console.log(Math.round(7.8));
+
+
+// var sqrtVal = Math.sqrt(81);
+// console.log(sqrtVal);
+
+
+// console.log(Math.sqrt(81));
+// console.log(Math.sqrt(9));
+// console.log(Math.sqrt(49));
+// console.log(Math.sqrt(8));
+
+
+// var randomVal = Math.random();
+// console.log(randomVal);
+
+// var test = randomVal * 5;
+// console.log(test);
+
+// var floorFormula = Math.floor(test) + 1;
+// console.log(floorFormula);
+
+
+// var ceilFormula = Math.ceil(test);
+// console.log(ceilFormula);
+
+
+// console.log(Math.ceil(54));
+// console.log(Math.ceil(6));
+// console.log(Math.ceil(2.3));
+// console.log(Math.ceil(9.921));
+// console.log(Math.ceil(10.123));
+
+
+
+// var piVal = Math.PI;
+// console.log(piVal);
+
+
+
+// console.log(Math.abs(4));
+// console.log(Math.abs(-9.5));
+// console.log(Math.abs(-213));
+// console.log(Math.abs(6));
+
+
+// let maxVal = Math.max(2, 5.12, 561, -91.5, 78, 3, 561.2, 100);
+// console.log(maxVal);
+
+
+
+// let minVal = Math.min(2, 5.12, 561, -91.5, 78, 3, 561.2, 100);
+// console.log(minVal);
+
+
+// console.log(Math.pow(3, 2));
+
+
+
+// var animals = ["lion", "tiger", "elephant", "zebra", "cat"];
+// console.log(animals);
+
+// var randomVal = Math.floor(Math.random() * animals.length);
+// console.log(randomVal);
+
+// console.log(animals[randomVal]);
+
+
+
+
+
+
+
+
+// var numVal = prompt("Enter any number!");
+// var numVal = 12.123;
+// numVal = Number(numVal);
+// numVal = parseInt(numVal);
+// console.log(numVal);
+// console.log(typeof numVal); 
+
+
+
+
+
+// var numToStr = 6;
+// // numToStr = String(numToStr);
+// numToStr = numToStr.toString();
+// console.log(numToStr, typeof numToStr);
+
+
+
+// var val = "1.999";
+// val = parseFloat(val);
+// console.log(val, typeof val);
+
+
+
+// var piVal = Math.PI;
+// console.log(piVal.toFixed(3));
---
+++ JavaScript/class-8/date-topic/app.js
@@ -0,0 +1,56 @@
+// var myDateOfB = "08 July 1996";
+
+// var rightNow = new Date();
+// console.log(rightNow);
+// console.log(typeof rightNow);
+
+// var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+// var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+// var today = rightNow.getDay();
+// console.log(days[today]);
+
+
+// var thisMonth = rightNow.getMonth();
+// console.log(months[thisMonth]);
+
+
+// var dateVal = rightNow.getDate();
+// console.log(dateVal);
+
+
+// var year = rightNow.getFullYear();
+// console.log(year);
+
+
+// var time = rightNow.getTime();
+// console.log(time);
+
+
+// var displayData = new Date(time);
+// console.log(displayData.toLocaleTimeString());
+// console.log(displayData.toLocaleDateString());
+// document.write(displayData.toLocaleDateString());
+
+
+
+
+
+
+
+
+// var rightNow = new Date();
+// // console.log(rightNow);
+
+// var birthYear = prompt("Enter your birth year!");
+// // console.log(birthYear);
+
+// var currentYear = rightNow.getFullYear();
+// // console.log(currentYear);
+
+// var calculateYear = currentYear - birthYear;
+// console.log(calculateYear);
+
+
+
+console.log(Date.now());
